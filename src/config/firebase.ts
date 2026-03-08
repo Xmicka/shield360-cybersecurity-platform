5361e4f02c9fc44ba88845bbb59b8d887148d931
Xmicka <akeshchandrasiri@gmail.com>
2026-03-08 21:08:17 +0530

    config: add Firebase credentials

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    apiKey: "AIzaSyDBh4N6jRKOL2z0e9M1Dq0lKWrFMsSS5v4",
+    authDomain: "behaviour-adapt-spear-phishing.firebaseapp.com",
+    projectId: "behaviour-adapt-spear-phishing",
+    storageBucket: "behaviour-adapt-spear-phishing.firebasestorage.app",
+    messagingSenderId: "35425075133",
+    appId: "1:35425075133:web:5409533f44d729771cdcd4",
 };
 
 const app = initializeApp(firebaseConfig);
